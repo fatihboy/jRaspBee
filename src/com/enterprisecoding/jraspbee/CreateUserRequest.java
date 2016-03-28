2462a6fc4a0aecf319eb222501fc01fa5d3bdebd
fatihboy <fatih@enterprisecoding.com>
2016-03-28 19:39:27 +0300

    Copyright headers added

--- src/com/enterprisecoding/jraspbee/CreateUserRequest.java
+++ src/com/enterprisecoding/jraspbee/CreateUserRequest.java
@@ -1,3 +1,10 @@
+/* jRaspBee - a Java library for RaspBee bridge
+ *  Copyright (c) 2016 Fatih Boy (http://enterprisecoding.com)
+ *  Licensed under the MIT (LICENSE).
+ *  
+ *  Forked from Jue (https://github.com/Q42/Jue) a java library
+ *  for managing Philips HUE Bridge
+ */
 package com.enterprisecoding.jraspbee;
 
 @SuppressWarnings("unused")
